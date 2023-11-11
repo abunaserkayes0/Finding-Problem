5f0e4d079d6eb8e2622f2072297d654940a09916
abunaserkayes1 <come74321@gmail.com>
2023-11-11 20:22:00 +0600

    Implements active Nav-Link

--- app/NavBar.jsx
+++ app/NavBar.jsx
@@ -1,20 +1,28 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { DiBugsense } from "react-icons/di";
+import classnames from "classnames";
 const NavBar = () => {
+  const currentPath = usePathname();
   const link = [
     { label: "Dashboard", href: "/" },
     { label: "Problems", href: "/problems" },
   ];
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/">
+      <Link href="/" className="text-5xl">
         <DiBugsense />
       </Link>
       <ul className="flex space-x-6">
         {link.map((link) => (
           <Link
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classnames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "text-zinc-500 hover:text-zinc-800 transition-colors": true,
+            })}
             key={link.href}
             href={link.href}
           >
---
+++ app/problems/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProblemsPages = () => {
+  return (
+    <div>ProblemsPages</div>
+  )
+}
+
+export default ProblemsPages
